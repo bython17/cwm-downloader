3d106eba27f8282c877aa6ef8f10a19ee8ff90e0
Bazliel Wondimagegn <92359779+bython17@users.noreply.github.com>
2022-08-20 21:45:40 +0300

    Updated selectors to use the elements found in lectures.
    Now the program doesn't access the course pages because of the
    freaking js that the site uses. Javascript doesnt seem to be renderable
    so switched to use the lecture pages for scraping the whole lectures
    needed to download a course

--- src/scraper/element_selectors.py
+++ src/scraper/element_selectors.py
@@ -3,18 +3,27 @@ from enum import Enum
 
 class ElementSelectors(Enum):
     course_name = [
-        "#__next > div > div > div.jsx-1640592643.wrap > h1",
-        "body > section > div.course-sidebar > h2"
+        # "#__next div div div.jsx-1640592643.wrap h1",
+        "body section div.course-sidebar h2"
+    ]
+    section_containers = [
+        # "body section div.course-mainbar div.row div",
+        "div.course-sidebar div.row.lecture-sidebar div.course-section"
     ]
     section_names = [
-        "#__next > div > div > div.jsx-4204861187.columns > div:nth-child(1) > div.slim-selection > div.jsx-4255369697.section-header > div:nth-child(1) > h2",
-        "body > section > div.course-mainbar > div.row > div > div"
+        "div.course-sidebar div.row.lecture-sidebar div.course-section div.section-title",
+        # "body section div.course-mainbar div.row div div"
     ]
-    lecture_names = [
-        "#__next > div > div > div.jsx-4204861187.columns > div:nth-child(1) > div.slim-selection > div.jsx-3137867545.lectures > div.jsx-4132056062.bar > a.jsx-4132056062.text > h3",
-        "body > section > div.course-mainbar > div.row > div > ul > li > a > div > span.lecture-name"
-    ]
+    lecture_name = ["#lecture_heading"]
+    lecture_icon = ['#lecture_heading svg use']
     lecture_anchor_tags = [
-        "#__next > div > div > div.jsx-4204861187.columns > div:nth-child(1) > div.slim-selection > div.jsx-3137867545.lectures > div.jsx-4132056062.bar > a.jsx-4132056062.text",
-        "body > section > div.course-mainbar > div.row > div > ul > li > a"
+        "div.course-sidebar div.row.lecture-sidebar div.course-section a.item",
+        "body section div.course-mainbar div.row div ul li a",
     ]
+    download_tags = ["a.download"]
+    main_container_div = ["body section div.course-mainbar.lecture-content.full-width-content.enforce-maximum-content-width"]
+    decomposable_elements = ['a.btn.complete.lecture-complete', '#empty_box', 'div.attachment-data', 'div.row attachment-pdf-embed']
+    attribute_cleared_meta_tag = ['#lecture-completion-data']
+
+
+print(ElementSelectors.section_containers.value)
